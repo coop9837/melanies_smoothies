9f57a4fdd4e9bb8bb32d26b3af0cd23df0bf7102
coop9837 <139181462+coop9837@users.noreply.github.com>
2025-07-18 13:43:40 -0500

    Update streamlit_app.py
    Testing New Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ st.write("The name on you Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-#st.dataframe(data = my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col('SEARCH_ON'))
+st.dataframe(data = my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     , my_dataframe
@@ -27,7 +28,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
